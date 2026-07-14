c19f4c5735ed3c8db9f925c79c07cd55c8b3c263
Андреј Трајковски <andrejtrajkovski03@gmail.com>
2026-07-14 16:12:33 +0200

    Initial commit

---
+++ src/main/java/mk/ukim/finki/gitcontributionanalyzer/GitContributionAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.gitcontributionanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitContributionAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitContributionAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/gitcontributionanalyzer/GitContributionAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.gitcontributionanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitContributionAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
